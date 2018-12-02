65a45eabe48854c09d341430e799e4495cba7dc3
nazari-ahmadi <e.ahmadi67@yahoo.com>
2018-12-02 19:17:39 +0330

    update  reports Component

--- plugins/sepehr/service/components/Reports.php
+++ plugins/sepehr/service/components/Reports.php
@@ -1,6 +1,7 @@
 <?php namespace Sepehr\Service\Components;
 
 use Cms\Classes\ComponentBase;
+use DB;
 use Sepehr\Details\Models\DistributionTime;
 use Sepehr\Details\Models\InsuranceType;
 use Sepehr\Details\Models\PackageType;
@@ -41,15 +42,39 @@ class Reports extends ComponentBase
     $sender_address = post('sender_address');
     $transaction_code = post('transaction_code');
     $weight_id = post('weight_id');
+    $receiver_postal_code = post('receiver_postal_code');
+    $receiver_address = post('receiver_address');
+    $post_type_id = post('post_type_id');
+    $distribution_time_id = post('distribution_time_id');
+    $special_services_id = post('special_services_id');
+    $package_type_id = post('package_type_id');
+    $insurance_type_id = post('insurance_type_id');
 
-    $services = Service::
-    where('sender_postal_code', 'like', "%$sender_postal_code%")->
-    where('sender_address', 'like', "%$sender_address%")->
-    where('transaction_code', 'like', "%$transaction_code%")->
-    whereIn()->
-    whereIn("weight_id",'like',"%$weight_id%")->
+//    "weight_id":"5455"
+//    "receiver_address": "تهران"
+//      "weight_id":"$weight_id"
 
-    get();
+    if ($weight_id != "0") {
+      $weight = "$weight_id";
+    } else {
+      $weight = null;
+    }
+
+
+
+    $services = DB::table('sepehr_service_index')
+      /* ->where('sender_postal_code', 'like', "%$sender_postal_code%")
+       ->where('sender_address', 'like', "%$sender_address%")
+       ->where('transaction_code', "$transaction_code")*/
+      ->where('packages', 'like', "%\"weight_id\":$weight%")
+      /* ->where('packages','like',"%\"receiver_postal_code\":\"$receiver_postal_code\"%")
+       ->where('packages','like',"%\"receiver_address\":\"$receiver_address\"%")
+       ->where('packages','like',"%\"post_type_id\":\"$post_type_id\"%")
+       ->where('packages','like',"%\"distribution_time_id\":\"$distribution_time_id\"%")
+       ->where('packages','like',"%\"special_services_id\":\"$special_services_id\"%")
+       ->where('packages','like',"%\"package_type_id\":\"$package_type_id\"%")
+       ->where('packages','like',"%    \"insurance_type_id\":\"$insurance_type_id\"%")*/
+      ->get();
     throw new \ApplicationException(json_encode($services));
   }
 
